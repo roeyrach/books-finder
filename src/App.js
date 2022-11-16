64de7aba6c5fc38b63513ce3a313b9bc0c34b74d
danielmargolin <margolin23@gmail.com>
2022-11-16 13:31:50 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import './App.css';
+
+import Home from './components/Home';
+import Search from './components/Search';
+import WishList from './components/WishList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/wishlist">Wish List</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/search" element={<Search/>}>
+          </Route>
+          <Route path="/wishlist" element={<WishList/>}>
+          </Route>
+          <Route path="/" element={<Home/>}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
