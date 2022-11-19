85d864e822129db689adbf39aa48b9cddd0d66fc
danielmargolin <margolin23@gmail.com>
2022-11-19 21:29:26 +0200

    added book component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React from "react";
+
+import Book from "./Book.js";
 
 const Search = () => {
-    return ( <div>Search</div> );
-}
- 
+  const books = [
+    {
+      volumeInfo: {
+        title: "The Contemporary Thesaurus of Search Terms and Synonyms",
+        authors: ["Sara D. Knapp"],
+        imageLinks: {
+          smallThumbnail:
+            "http://books.google.com/books/content?id=zYw3sYFtz9kC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+          thumbnail:
+            "http://books.google.com/books/content?id=zYw3sYFtz9kC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        },
+      },
+    },
+    {
+      volumeInfo: {
+        title: "Hands-on Information Literacy Activities",
+        authors: ["Jane Birks", "Fiona Hunt"],
+        imageLinks: {
+          smallThumbnail:
+            "http://books.google.com/books/content?id=77RZAAAAYAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+          thumbnail:
+            "http://books.google.com/books/content?id=77RZAAAAYAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+        },
+      },
+    },
+  ];
+
+  return (
+    <div>
+      {books.map(book => <Book {...book} />)}
+    </div>
+  );
+};
+
 export default Search;
